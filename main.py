bd16b53e18450cdf2c0ded29bf7b24eb11e9de41
Thomas McCall <thomas.e.mccall@gmail.com>
2021-01-30 15:29:14 -0600

    read in shelter name as arg, process and save data to CSV

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import sys
+import csv
 from requests_html import HTMLSession
 
 session = HTMLSession()
+print("Successfully set up HTML session!")
 
-r = session.get('https://www.jrspupsnstuff.org/dogs')
+# pass the shelter's dog listing URL as a script arg
+print("Gathering dog data...")
+r = session.get(sys.argv[1])
 petango_link_iframe = r.html.find("iframe")
 petango_link = petango_link_iframe[0].attrs['src']
 
@@ -11,10 +16,15 @@ petango_link = petango_link_iframe[0].attrs['src']
 r = session.get(petango_link)
 animal_info_block_list_raw = r.html.find("div.list-animal-info-block")
 
-# loop through the dogs
-#dogs = []
-#for a in animal_info_block_list_raw:
-#    dogs.append(a.text.splitlines())
-
 dogs = [a.text.splitlines() for a in animal_info_block_list_raw]
-print([d[0] for d in dogs])
+print("Names: ", [d[0] for d in dogs])
+
+# save the data off to a CSV
+print("Writing saved dog data to a file...")
+shelter_name = sys.argv[1].split("//")[1].split("/")[0].split(".")[-2]
+with open(f'{shelter_name}_data.csv', mode='w') as dog_file:
+    dog_writer = csv.writer(dog_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    for dog in dogs:
+        dog_writer.writerow(dog)
+print("Done.")
